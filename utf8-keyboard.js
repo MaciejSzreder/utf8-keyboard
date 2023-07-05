df4eca72277e3c5e02b7fd6ae4fe513f6ce390b8
MaciejSzreder <MSzreder3.14@wp.pl>
2023-07-06 00:48:22 +0200

    introduced class for keyboard

--- utf8-keyboard.js
+++ utf8-keyboard.js
@@ -1,42 +1,42 @@
-{
+class Keyboard{
+	text='';
+	key_map = 0;
+	key_dir = {
+		a: 128,
+		s:  64,
+		d:  32,
+		f:  16,
+		j:   8,
+		k:   4,
+		l:   2,
+		';': 1,
+	};
+	firstUp = false;
+	textarea = document.getElementById`textarea`;
 
-let textarea = document.getElementById`textarea`;
-let key_map = 0;
-let key_dir = {
-	a: 128,
-	s:  64,
-	d:  32,
-	f:  16,
-	j:   8,
-	k:   4,
-	l:   2,
-	';': 1,
-};
-
-let text='';
-function preview(char){
-	textarea.value = text+char;
-}
-function write(char){
-	text += char;
-	textarea.value = text;
-}
-
-let firstUp = false;
-function keyDown(e){
-	e.preventDefault();
-	key_map|=key_dir[e.key];
-	preview(String.fromCharCode(key_map));
-	firstUp = true
-}
-function keyUp(e){
-	e.preventDefault();
-	if(firstUp){
-		firstUp = false;
-		write(String.fromCharCode(key_map));
+	preview(char){
+		textarea.value = this.text+char;
 	}
-	key_map&=~key_dir[e.key];
-	preview(String.fromCharCode(key_map));
-}
 
+	write(char){
+		this.text += char;
+		textarea.value = this.text;
+	}
+
+	keyDown(e){
+		e.preventDefault();
+		this.key_map|=this.key_dir[e.key];
+		this.preview(String.fromCharCode(this.key_map));
+		this.firstUp = true
+	}
+	
+	keyUp(e){
+		e.preventDefault();
+		if(this.firstUp){
+			this.firstUp = false;
+			this.write(String.fromCharCode(this.key_map));
+		}
+		this.key_map&=~this.key_dir[e.key];
+		this.preview(String.fromCharCode(this.key_map));
+	}
 }
